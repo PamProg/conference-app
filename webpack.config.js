b0a721babcf940d513c2428e9e49608e18c1d8e0
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-08-21 17:49:48 +0200

    ajout bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 let path = require("path");
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 
+var webpack = require("webpack");
 
 module.exports = {
     entry: './src/index.js',
@@ -9,11 +10,25 @@ module.exports = {
         filename: "bundle.js"
     },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            Popper:'popper.js'
+        })
     ],
     module: {
         rules: [
-            {test: /\.(html)$/, use: "html-loader"}
+            {test: /\.(html)$/, use: "html-loader"},
+            {test: /\.(jpg)$/, use: "file-loader"},
+            {test: /\.(css)$/, use: [
+                "style-loader", "css-loader"
+            ]},
+            {
+                test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: 'file-loader?name=fonts/[name].[ext]'
+            }
         ]
     }
 };
